6f1ec678b0d17c65747c47b8f31e83d6ba02b52e
Pol-S <plsbnjy@gmail.com>
2021-02-07 12:20:44 -0600

    Got a basic API call going and a basic display of some data going

--- src/api/CovidTracking.js
+++ src/api/CovidTracking.js
@@ -1,11 +1,32 @@
 import React from "react"
+import axios from "axios"
 
-function CovidTracking () {
-  return (
-    <div>
-      Api call goes here
-    </div>
-  )
+class CovidTracking extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      covidData: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.covidtracking.com/v1/states/ca/20200501.json')
+      .then(response => {
+        this.setState({
+          covidData: response.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.covidData.state}<br></br>
+        {this.state.covidData.date}<br></br>
+        Api call goes here
+      </div>
+    )
+  }
 }
 
 export default CovidTracking
