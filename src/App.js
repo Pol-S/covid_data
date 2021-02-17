df145eb833f33383f5286f2978014cb7f326be7e
sam <sblan45@gmail.com>
2021-02-16 20:55:18 -0500

    adds all US states to states category and modifies hospitalized on Y axis and adds death increase to y axis

--- src/App.js
+++ src/App.js
@@ -64,9 +64,55 @@ class App extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label>Choose a State:
             <select value={this.state.usState} onChange={this.handleChange}>
+              <option value='al'>Alabama</option>
+              <option value='ak'>Alaska</option>
+              <option value='az'>Arizona</option>
+              <option value='ar'>Arkansas</option>
               <option value='ca'>California</option>
-              <option value='ma'>Massachusetts</option>
+              <option value='co'>Colorado</option>
+              <option value='ct'>Connecticut</option>
+              <option value='de'>Delaware</option>
+              <option value='fl'>Florida</option>
+              <option value='ga'>Georgia</option>
+              <option value='hi'>Hawaii</option>
+              <option value='id'>Idaho</option>
               <option value='il'>Illinois</option>
+              <option value='in'>Indiana</option>
+              <option value='ia'>Iowa</option>
+              <option value='ks'>Kansas</option>
+              <option value='ky'>Kentucky</option>
+              <option value='la'>Louisiana</option>
+              <option value='me'>Maine</option>
+              <option value='md'>Marlyand</option>
+              <option value='ma'>Massachusetts</option>
+              <option value='mi'>Michigan</option>
+              <option value='mn'>Minnesota</option>
+              <option value='mo'>Missouri</option>
+              <option value='mt'>Montana</option>
+              <option value='ne'>Nebraska</option>
+              <option value='nv'>Nevada</option>
+              <option value='nh'>New Hampshire</option>
+              <option value='nj'>New Jersey</option>
+              <option value='nm'>New Mexico</option>
+              <option value='ny'>New York</option>
+              <option value='nc'>North Carolina</option>
+              <option value='nd'>North Dakota</option>
+              <option value='oh'>Ohio</option>
+              <option value='ok'>Oklahoma</option>
+              <option value='or'>Oregon</option>
+              <option value='pa'>Pennsylvania</option>
+              <option value='ri'>Rhode Island</option>
+              <option value='sc'>South Carolina</option>
+              <option value='sd'>South Dakota</option>
+              <option value='tn'>Tennessee</option>
+              <option value='tx'>Texas</option>
+              <option value='ut'>Utah</option>
+              <option value='vt'>Vermont</option>
+              <option value='va'>Virginia</option>
+              <option value='wa'>Washington</option>
+              <option value='wv'>West Virginia</option>
+              <option value='wi'>Wisconsin</option>
+              <option value='wy'>Wyoming</option>
             </select>
           </label> 
         </form>    
@@ -76,6 +122,7 @@ class App extends React.Component {
               <option value='total'>Total</option>
               <option value='hospitalized'>Hospitalized</option>
               <option value='death'>Death</option>
+              <option value='deathIncrease'>Death Increase</option>
             </select>
           </label> 
         </form>  
